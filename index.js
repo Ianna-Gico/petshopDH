0b3e1ebe1b13f963e58bb4453b18f28cd4c675e6
ianna.gico@gmail.com <ianna.gico.silva@avanade.com>
2021-03-25 23:57:33 -0300

    todos os desafios (utilizando moment.js)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 const nomePetshop = "PETSHOP DH";
 
 let pets = [
@@ -10,7 +12,7 @@ let pets = [
         tutor: 'Charlie Brown',
         contato: '(81) 99999-9999',
         vacinado: true,
-        servicos: ['banho', 'tosa']
+        servicos: []
     },
     {
         nome: 'Biro-Biro',
@@ -21,7 +23,7 @@ let pets = [
         tutor: 'Jojô',
         contato: '(83) 99999-9999',
         vacinado: false,
-        servicos: ['banho', 'tosa']
+        servicos: []
     },
     {
         nome: 'Yani',
@@ -32,20 +34,83 @@ let pets = [
         tutor: 'Pucca',
         contato: '(11) 99999-9999',
         vacinado: true,
-        servicos: ['banho', 'tosa', 'corte de unha']
+        servicos: []
     }
-
 ];
 
-const listarPets = () => {
-    for(let pet of pets){
-
-        //console.log(pets[i].nome + " " + pets[i].raca);
-        console.log(`O nome do pet é ${pet.nome + " e sua raça é " + pet.raca}`);
+const vacinarPet = (pet) => {
+    if (pet.vacinado == false){
+        pet.vacinado = true;
+        console.log(`O pet ${pet.nome} foi vacinado com sucesso!`)
+    }else{
+        console.log(`Ops! O pet ${pet.nome} já está vacinado!`)
     }
-
 }
 
+const campanhaVacina = () => {
+    var soma = 0;
+    for(let pet of pets){
+        if(pet.vacinado == false){
+            pet.vacinado == true;
+            soma++;
+        }
+    }
+    console.log(`${soma} pet(s) foi(ram) vacinado(s) nesta campanha!`)
+}
+
+const adicionarPet = (pet) => {
+    pets.push(pet);
+}
+
+const listarPets = () => {
+    for(var pet of pets){
+        console.log(`O nome do pet é ${pet.nome} e sua raça é ${pet.raca} (${JSON.stringify(pet.servicos)})`);
+    }
+}
+
+const prestarServico = (pet, servico, data) => {
+    pet.servicos.push({
+        servico: servico,
+        data: data
+    });
+}
+
+const darBanhoPet = (pet) => {
+    prestarServico(pet, 'banho', moment().format('L'));
+    console.log(`${pet.nome} está de banho tomado!`);
+}
+
+const tosarPet = (pet) => {
+    prestarServico(pet, 'tosa', moment().format('L'));
+    console.log(`${pet.nome} está com o cabelinho na régua!`);
+}
+
+const apararUnhasPet = (pet) => {
+    prestarServico(pet, 'corte de unhas', moment().format('L'));
+    console.log(`${pet.nome} está de unhas aparadas!`);
+}
+
+adicionarPet({
+    nome: 'Jk',
+    tipo: 'cachorro',
+    idade: 5,
+    raca: 'Vira-lata',
+    peso: 3,
+    tutor: 'Doug',
+    contato: '(81) 98145-7456',
+    vacinado: false,
+    servicos: []
+});
+vacinarPet(pets[0]);
+vacinarPet(pets[1]);
+campanhaVacina();
+listarPets();
+darBanhoPet(pets[2]);
+darBanhoPet(pets[3]);
+listarPets();
+tosarPet(pets[2]);
+apararUnhasPet(pets[3]);
 listarPets();
 
-//console.log(pets);
+prestarServico(pets[0], 'banho', moment().format('L'));
+listarPets();
